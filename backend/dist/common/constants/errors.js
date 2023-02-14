7474dbb21f2755feab7633060e5c7fd57b0cacbd
VadymStratiienko <101202902+VadymStratiienko@users.noreply.github.com>
2023-02-14 18:44:17 +0200

    Delete backend/dist directory

--- backend/dist/common/constants/errors.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AppError = void 0;
-exports.AppError = {
-    USER_EXIST: 'User this email exists',
-    USER_NOT_EXIST: 'User with this email not exist',
-    WRONG_DATA: 'Wrong data',
-};
-//# sourceMappingURL=errors.js.map
--- backend/dist/modules/watchlist/watchlist.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { Watchlist } from './models/watchlist.model';
-import { CreateAssetResponse } from './response';
-export declare class WatchlistService {
-    private readonly watchlistRepository;
-    constructor(watchlistRepository: typeof Watchlist);
-    createAsset(user: any, dto: any): Promise<CreateAssetResponse>;
-    deleteAsset(userId: number, assetId: string): Promise<boolean>;
-}
