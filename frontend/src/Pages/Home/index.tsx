df4a448104ed5caa9fd71bc0672b14c1403e7b10
VadymStratiienko <VadymStratiienko44@gmail.com>
2023-02-28 13:35:08 +0200

    fix

--- frontend/src/Pages/Home/index.tsx
+++ frontend/src/Pages/Home/index.tsx
@@ -1,24 +1,32 @@
-import React, {useCallback, useEffect, useMemo, useRef} from 'react';
+import React, {FC, useCallback, useEffect, useMemo, useRef} from 'react';
 import {useAppDispatch, useAppSelector} from "../../utils/hook";
 import {getFavoriteAssets} from "../../store/thunks/assets";
 import {Box, Grid} from "@mui/material";
+import TrendUp from '../../assets/images/chart/trend-up.svg'
+import TrendDown from '../../assets/images/chart/trend-down.svg'
 import {useStyles} from "./styles";
 
-const Home = () => {
-    const favoriteAssets: any[] = useAppSelector(state => state.assets.favoriteAssets)
+const Home: FC = (): JSX.Element => {
+    const favoriteAssets: any[] = useAppSelector(
+        (state) => state.assets.favoriteAssets,
+    )
     const dispatch = useAppDispatch()
     const fetchDataRef = useRef(false)
     const classes = useStyles()
 
     const favoriteAssetName = useMemo(() => ['bitcoin', 'ethereum'], [])
-    const filteredArray = favoriteAssets.filter((value, index, self) => index === self.findIndex((t) => t.name === value.name)
+    const filteredArray = favoriteAssets.filter(
+        (value, index, self) =>
+            index === self.findIndex((t) => t.name === value.name),
     )
-    const fetchData = useCallback((data: string[]) => {
-        data.forEach((element: string) => {
-            dispatch(getFavoriteAssets(element))
-        })
-    }, [dispatch])
-
+    const fetchData = useCallback(
+        (data: string[]) => {
+            data.forEach((element: string) => {
+                dispatch(getFavoriteAssets(element))
+            })
+        },
+        [dispatch],
+    )
 
     useEffect(() => {
         if (fetchDataRef.current) return
@@ -27,20 +35,42 @@ const Home = () => {
     }, [favoriteAssetName, fetchData])
 
     const renderFavoriteBlock = filteredArray.map((element: any) => {
-        const currentPrice = element.data.prices[0]
-        const currentCap = element.data.market_caps[0]
+        const currentPrice = element.singleAsset.map(
+            (element: any) => element.current_price,
+        )
+        const currentCap = element.singleAsset.map(
+            (element: any) => element.market_cap,
+        )
+        const changePrice = element.singleAsset.map(
+            (element: any) => element.price_change_percentage_24h,
+        )
         return (
-            <Grid item xs={12} sm={6} lg={6}>
+            <Grid item xs={12} sm={6} lg={6} key={element.name}>
                 <Grid container className={classes.topCardItem}>
                     <Grid item xs={12} sm={6} lg={6}>
                         <h3 className={classes.assetName}>{element.name}</h3>
                         <div className={classes.itemDetails}>
-                            <h3 className={classes.cardPrice}>${currentPrice[1].toFixed(2)}</h3>
-                            <p className={classes.cardCapitalize}>${currentCap[1].toFixed(0)}</p>
+                            <h3 className={classes.cardPrice}>
+                                ${currentPrice}
+                            </h3>
+                            <Box
+                                className={
+                                    changePrice > 0
+                                        ? `${classes.priceTrend} ${classes.trendUp}`
+                                        : `${classes.priceTrend} ${classes.trendDown}`
+                                }
+                            >
+                                {changePrice > 0 ? (
+                                    <img src={TrendUp} alt="TrendUp" />
+                                ) : (
+                                    <img src={TrendDown} alt="TrendDown" />
+                                )}
+                                <span>{Number(changePrice).toFixed(2)}%</span>
+                            </Box>
                         </div>
                     </Grid>
                     <Grid item xs={12} sm={6} lg={6}>
-                        <h5>Chart</h5>
+                        <AreaChart data={element.data} />
                     </Grid>
                 </Grid>
             </Grid>
@@ -53,7 +83,7 @@ const Home = () => {
                 {renderFavoriteBlock}
             </Grid>
         </Box>
-    );
-};
+    )
+}
 
-export default Home;
+export default Home
